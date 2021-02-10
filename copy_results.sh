e960e7d3de0fda5534ef71864a2b1d8e71ab059e
kingusiu <3071710+kingusiu@users.noreply.github.com>
2021-02-11 00:41:42 +0100

    removing shell scripts

--- copy_results.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "creating directory for quantile $1 and strategy $2"
-export RES_DIR="$1_$2_results"
-mkdir ${RES_DIR}
-mv *.root ${RES_DIR}
-mv *.txt ${RES_DIR}
-mv pvalue.png ${RES_DIR}
-
--- setupenv.sh
+++
@@ -1,7 +0,0 @@
-cd /afs/cern.ch/user/k/kiwoznia/CMSSW_10_2_3/src
-eval `scramv1 runtime -sh`
-#cmsenv
-export PYTHONPATH=
-export PYTHON27PATH=$PYTHON27PATH:/afs/cern.ch/user/k/kiwoznia/.local/lib/python2.7/site-packages
-cd -
-
